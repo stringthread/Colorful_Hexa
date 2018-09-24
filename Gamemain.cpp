37d07d00fdce173e4eae9add1034e04091a658cb
stringthread <string.thread@gmail.com>
2018-09-24 13:22:16 +0900

    Initial commitment

---
+++ Gamemain.cpp
@@ -0,0 +1,480 @@
+#include "stdafx.h"
+#include "ColorfulHexa.h"
+#include <cmath>
+#include <sstream>
+#include <ios>
+#include <iomanip>
+
+void Gamemain::change_mode() {
+	switch (mode) {
+		case M_TITLE:
+			mode = M_CHOOSE;
+			break;
+		case M_CHOOSE:
+			t_load = thread([this]() {bmsm.load_music(render);});
+			t_load.detach();
+			step_choose = 0;
+			mode = M_LOADING;
+			break;
+		case M_LOADING:
+			play_start = timer->get_time();
+			play_time = 0l;
+			mode = M_PLAY;
+			break;
+		case M_PLAY:mode = M_RESULT; break;
+		case M_RESULT:
+			bmsm.reset();
+			qr.reset();
+			pushing_long.fill(0);
+			judge.fill(0);
+			score = 0;
+			combo = 0;
+			max_combo = 0;
+			colors = 0;
+			colorful_gauge = 0.0f;
+			curr_judge = -1;
+			mode = M_TITLE;
+			break;
+	}
+	return;
+}
+
+void Gamemain::calc_score() {
+	score = (long)(100000 / bmsm.get_num_note()*(judge[0] * S_PERFECT + judge[1] * S_GREAT + judge[2] * S_GOOD));
+	score += (long)(10000 / bmsm.get_num_note() *max_combo);
+	score += (long)(10000.0f * colorful_gauge/100);
+	/*
+	if (colorful_gauge >= 1.0f) {
+		if (colors < 5) {
+			colors++;
+			colorful_gauge = 0.0f;
+		} else colorful_gauge = 1.0f;
+	}
+	*///no color back
+	///*
+	if(colorful_gauge>=6.0f){
+	colorful_gauge=5.999f;
+	}
+	colors = (int)colorful_gauge;
+	//*///color back
+}
+
+void Gamemain::draw_note(long count,int lane,int color,long end_count){
+	if (lane < 0 || lane >= 6)return;
+
+	float dis_note1,dis_note2;
+	if (count < bmsm.time_to_count(play_time) && end_count < bmsm.time_to_count(play_time))return;
+
+	if (end_count > 0) {
+		SDL_SetRenderDrawColor(render, 0, 255, 0, 16);
+		dis_note1 = (0.3f + 0.7f*max(count - bmsm.time_to_count(play_time), 0) / (bmsm.time_to_count(play_time + bmsm.time_draw_start) - bmsm.time_to_count(play_time)))*scr_h / 2;
+		dis_note2 = min(0.3f + 0.7f*(end_count - bmsm.time_to_count(play_time)) / (bmsm.time_to_count(play_time + bmsm.time_draw_start) - bmsm.time_to_count(play_time) - NOTE_W), 1)*scr_h / 2;
+		rect_note.h = (dis_note2 - dis_note1)*sin(pi/3)+1;
+		rect_note.w = rect_note.h*size_ln[0];
+		rect_note.x = scr_w / 2 + sin(pi*lane / 3)*dis_note1-rect_note.w;
+		rect_note.y = scr_h / 2 - cos(pi*lane / 3)*dis_note1-rect_note.h;
+		center.x = rect_note.w;
+		center.y = rect_note.h;
+		SDL_RenderCopyEx(render, ln[color][0], NULL, &rect_note, 60 * (lane+0.5f), &center, SDL_FLIP_NONE);
+		//first triangle
+
+		rect_note.x = scr_w / 2 + sin(pi*(lane+1) / 3)*dis_note1;
+		rect_note.y = scr_h / 2 - cos(pi*(lane+1) / 3)*dis_note1 - rect_note.h;
+		center.x = 0;
+		center.y = rect_note.h;
+		SDL_RenderCopyEx(render, ln[color][2], NULL, &rect_note, 60 * (lane+0.5f), &center, SDL_FLIP_NONE);
+		//second triangle
+
+		rect_note.w = dis_note1+1;
+		rect_note.h = (dis_note2 - dis_note1)*sin(pi / 3);
+		rect_note.x = scr_w / 2 + sin(pi*lane / 3)*dis_note1;
+		rect_note.y = scr_h / 2 - cos(pi*lane / 3)*dis_note1 - rect_note.h;
+		center.y = rect_note.h;
+		SDL_RenderCopyEx(render, ln[color][1], NULL, &rect_note, 60 * (lane+0.5f), &center, SDL_FLIP_NONE);
+		//center rect
+
+		if(count>bmsm.time_to_count(play_time))draw_note(count, lane, 6);
+		if (end_count < bmsm.time_to_count(play_time + bmsm.time_draw_start))draw_note(end_count, lane, 6);
+		return;
+	}
+	
+	dis_note1 = ((0.3f + 0.7f*(count - bmsm.time_to_count(play_time)) / (bmsm.time_to_count(play_time + bmsm.time_draw_start) - bmsm.time_to_count(play_time)))/2+NOTE_W)*scr_h;
+	dis_note2 = (0.3f + 0.7f*(count - bmsm.time_to_count(play_time)) / (bmsm.time_to_count(play_time + bmsm.time_draw_start) - bmsm.time_to_count(play_time))) / 2*scr_h;
+
+	rect_note.w = int(sin(pi / 3)*dis_note2);
+	rect_note.h = abs(dis_note2 - int(cos(pi/3)*dis_note1));
+	rect_note.x = scr_w/2+int(sin((lane + 0.5f)*pi / 3)*(dis_note1 + dis_note2) / 2)-rect_note.w/2;
+	rect_note.y = scr_h/2-int(cos((lane + 0.5f)*pi / 3)*(dis_note1 + dis_note2) / 2)-rect_note.h/2;
+
+	SDL_RenderCopyEx(render, note[color], NULL, &rect_note, 60 * lane, NULL, SDL_FLIP_NONE);
+}
+
+bool Gamemain::check_judge(long count) {
+	long now_count = bmsm.time_to_count(play_time);
+	long judge_count[4] = {
+		bmsm.time_to_count(play_time + R_PERFECT * 1000 / FPS) - now_count,
+		bmsm.time_to_count(play_time + R_GREAT * 1000 / FPS) - now_count,
+		bmsm.time_to_count(play_time + R_GOOD * 1000 / FPS) - now_count,
+		bmsm.time_to_count(play_time + R_MISS * 1000 / FPS) - now_count,
+	};
+	long dis_count = abs(count - now_count);
+	for (int i= 0; i < 4; i++) {
+		if (dis_count > judge_count[i])continue;
+		judge[i]++;
+		curr_judge = i;
+		//judgement
+
+		if (i == 0)curr_timing = T_NONE;
+		else if (count > now_count)curr_timing = T_FAST;
+		else curr_timing = T_LATE;
+		//timing
+
+		if (i == 3)combo = 0;
+		else {
+			combo++;
+			if (combo > max_combo)max_combo = combo;
+		}
+		//combo
+
+		colorful_gauge += COL_GAUGE[i];
+		if (colorful_gauge < 0.0f)colorful_gauge = 0.0f;
+		//colorful gauge
+		return true;
+	}
+	return false;
+}
+
+string Gamemain::digit(int data, int n) {
+	ostringstream oss;
+	oss << setw(n) << setfill('0') << data;
+	return oss.str();
+}
+
+string Gamemain::digit(float data, int n) {
+	ostringstream oss;
+	oss << fixed << setprecision(n) << data;
+	return oss.str();
+}
+
+int Gamemain::title(){
+	if (bmsm.header_loaded) {
+		if (any_pressed() || qr.read()) {
+			change_mode();
+		}
+		SDL_SetRenderDrawColor(render, 0xaa, 0xaa, 0xff, SDL_ALPHA_OPAQUE);
+		SDL_RenderClear(render);
+		//draw title screen
+	} else {
+		SDL_SetRenderDrawColor(render, 0xff, 0xaa, 0xff, SDL_ALPHA_OPAQUE);
+		SDL_RenderClear(render);
+		//draw opening screen
+	}
+	return 0;
+}
+
+int Gamemain::choose(){
+	SDL_RenderCopy(render,bmsm.get_stagefile(),NULL,&rect_stagefile);
+	switch (step_choose) {
+		case 0:
+			if (input[0] == -1 || input[5] == -1)step_choose++;
+			if (input[1]==1 || (input[1] >= 60 && (input[1] - 60) % 20 == 0)) {
+				bmsm.change_sel(1);
+			}
+			if (input[4]==1 || (input[4] >= 60 && (input[4] - 60) % 20 == 0)) bmsm.change_sel(-1);
+			
+			SDL_RenderCopyAlpha(render, bmsm.get_stagefile(-1), NULL, &rect_l,180);
+			SDL_RenderCopyAlpha(render, bmsm.get_stagefile(1), NULL, &rect_r, 180);
+			//draw choose song screen
+			break;
+			//choose song
+		case 1:
+			if (input[2] == -1 || input[3] == -1)step_choose--;
+			if (input[0] == -1 || input[5] == -1)step_choose++;
+			if (input[1]==1 || (input[1] >= 60 && (input[1] - 60) % 20 == 0)) bmsm.change_lv(1);
+			if (input[4]==1 || (input[4] >= 60 && (input[4] - 60) % 20 == 0)) bmsm.change_lv(-1);
+
+			s_letter = TTF_RenderText_Blended(font, (" "+to_string(bmsm.get_lv())+" ").c_str(), f_color);
+			letter = SDL_CreateTextureFromSurface(render, s_letter);
+			SDL_FreeSurface(s_letter);
+			SDL_RenderCopy(render, letter, NULL, &rect_center);
+			SDL_DestroyTexture(letter);
+			//draw choose lv screen
+			break;
+			//choose lv
+		case 2:
+			if (input[2] == -1 || input[3] == -1)step_choose--;
+			if (input[0] == -1 || input[5] == -1)step_choose++;
+			if (input[1]==1 || (input[1] >= 60 && (input[1] - 60) % 20 == 0)) bmsm.change_speed(0.25f);
+			if (input[4]==1 || (input[4] >= 60 && (input[4] - 60) % 20 == 0)) bmsm.change_speed(-0.25f);
+
+			s_letter = TTF_RenderText_Blended(font, digit(bmsm.get_speed(),2).c_str(), f_color);
+			letter = SDL_CreateTextureFromSurface(render, s_letter);
+			SDL_FreeSurface(s_letter);
+			SDL_RenderCopy(render, letter, NULL, &rect_center);
+			SDL_DestroyTexture(letter);
+			//draw choose speed screen
+			break;
+			//choose speed
+		case 3:
+			if (input[2] == -1 || input[3] == -1) {
+				step_choose--;
+				break;
+			}
+			if (any_pressed()) {
+				step_choose = 0;
+				change_mode();//start loading data
+				break;
+			}
+
+			//draw confirm screen
+			break;
+			//confirm
+	}
+	return 0;
+}
+
+int Gamemain::loading() {
+	if (bmsm.music_loaded) {
+		if (any_pressed()) {
+			bmsm.music_loaded = false;
+			change_mode();
+		}
+		SDL_SetRenderDrawColor(render, 0xff, 0xaa, 0xaa,SDL_ALPHA_OPAQUE);
+		SDL_RenderClear(render);
+		//draw play start screen
+	}else{
+
+		//draw loading screen
+	}
+	return 0;
+}
+
+int Gamemain::play(){
+	play_time = (long)(timer->get_time() - play_start);
+	if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_ESCAPE] == 1)change_mode();
+	bmsm.get_notes(&notes[0],play_time);
+	for (int i = 0; i < 7; i++) {
+		if (notes[i].size() == 0)continue;
+		for (int j = 0; j < notes[i].size(); j++) {
+			if (i == 0 && !notes[i][j].pushed) {
+				if(bmsm.time_to_count(play_time)<2*bmsm.BAR_STANDARD){
+					ch = Mix_GroupAvailable(7);
+					if (ch != -1)goto play;
+				}
+				ch = Mix_GroupAvailable(6);
+				if (ch == -1) {
+					ch = Mix_GroupOldest(6);
+					if (Mix_Playing(ch)==1)Mix_HaltChannel(ch);
+				}
+				play:
+				Mix_PlayChannel(ch, bmsm.get_wav(notes[i][j].get_data()), 0);
+				bmsm.set_pushed(0, notes[i][j].get_index());
+			}
+			//bgm
+			long now_cnt = bmsm.time_to_count(play_time - R_GOOD * 1000 / FPS);
+			if (notes[i][j].get_count() < bmsm.time_to_count(play_time - R_GOOD * 1000 / FPS)) {
+				if (i != 0) {
+					if (!notes[i][j].pushed) {
+						combo = 0;
+					}
+					colorful_gauge += COL_GAUGE[3];
+					if (colorful_gauge < 0.0f)colorful_gauge = 0.0f;
+				}
+				if(notes[i][j].get_end_count()<bmsm.time_to_count(play_time - R_GOOD * 1000 / FPS))bmsm.set_next_note(notes[i][j].is_ln() ? i + 13 : i + 7);
+				continue;
+			}
+			if(notes[i][j].pushed&&notes[i][j].get_count() < bmsm.time_to_count(play_time)&& notes[i][j].get_end_count() < bmsm.time_to_count(play_time)) {
+				bmsm.set_next_note(notes[i][j].is_ln() ? i + 13 : i + 7);
+				continue;
+			}
+			//check if note in range
+
+			if (i == 0)continue;
+			if (input[i-1] == 1) {
+				if (notes[i][j].pushed)continue;
+				if (notes[i][j].get_end_count() != -1)pushing_long[i-1] = true;
+
+				ch = Mix_GroupAvailable(i - 1);
+				if (ch == -1) {
+					ch = Mix_GroupOldest(i - 1);
+					if(Mix_Playing(ch)==1)Mix_HaltChannel(ch);
+				}
+				Mix_PlayChannel(ch,bmsm.get_wav(notes[i][j].get_data()),0);
+				//audio
+
+				if (check_judge(notes[i][j].get_count()))bmsm.set_pushed(i + 7, notes[i][j].get_index());
+			}
+			break;
+		}
+	}
+	calc_score();
+	if (bmsm.get_total_count() < bmsm.time_to_count(play_time))change_mode();
+	//calculation
+
+	SDL_SetRenderDrawColor(render, COLOR_HEX[colors][0], COLOR_HEX[colors][1], COLOR_HEX[colors][2], SDL_ALPHA_OPAQUE);
+	//rect_col.h = (int)(1.732f/3*2*rect_col.w*colorful_gauge);//no color back
+	rect_col.h = (int)(1.732f / 3 * 2*rect_col.w*(colorful_gauge-(int)colorful_gauge));//color back
+	rect_col.y = scr_h / 2 + (int)(1.732f/3*rect_col.w) - rect_col.h;
+	SDL_RenderFillRect(render,&rect_col);
+
+	for (int i= 0; i < 6; i++) {
+		for (int j = 0; j < notes[i+1].size(); j++) {
+			draw_note(notes[i+1][j].get_count(),i,colors,notes[i+1][j].get_end_count());
+		}
+		SDL_RenderCopy(render,(input[i] > 0) ? btn_p[i] : btn[i], NULL,  &rect_btn[i]);
+	}
+	s_letter = TTF_RenderUTF8_Blended(font, digit((int)score, 6).c_str(), f_color);
+	letter = SDL_CreateTextureFromSurface(render, s_letter);
+	SDL_FreeSurface(s_letter);
+	SDL_RenderCopy(render,letter, NULL, &rect_score);
+	SDL_DestroyTexture(letter);
+	s_letter = TTF_RenderUTF8_Blended(font, digit((int)combo,3).c_str(), f_color);
+	letter = SDL_CreateTextureFromSurface(render, s_letter);
+	SDL_FreeSurface(s_letter);
+	SDL_RenderCopy(render,letter, NULL, &rect_combo);
+	SDL_DestroyTexture(letter);
+	//draw play screen
+	return 0;
+}
+
+int Gamemain::result(){
+	if (any_pressed() && !qr.read())change_mode();
+	SDL_SetRenderDrawColor(render, 0xaa, 0xff, 0xaa, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(render);
+	//draw result screen
+	return 0;
+}
+
+void Gamemain::init(){
+	TTF_Init();
+	char c_curr_dir[256] = {};
+	string curr_dir;
+	GetModuleFileName(NULL, &c_curr_dir[0], 256);
+	curr_dir=regex_replace(c_curr_dir, regex("\\\\[^\\\\]+$"), "");
+	font = TTF_OpenFont((curr_dir + "\\font\\font.ttf").c_str(), 200);
+	//font
+
+	rect_btn[0].x = scr_w / 2; rect_btn[0].y = int(0.35f*scr_h); rect_btn[0].w = int(0.15f*scr_w); rect_btn[0].h = int(0.1125f*scr_h);
+	rect_btn[1].x = int(0.575f*scr_w); rect_btn[1].y = int(0.425f*scr_h); rect_btn[1].w = int(0.075f*scr_w); rect_btn[1].h = int(0.15f*scr_h);
+	rect_btn[2].x = rect_btn[0].x; rect_btn[2].y = int(0.5375f*scr_h); rect_btn[2].w = rect_btn[0].w; rect_btn[2].h = rect_btn[0].h;
+	rect_btn[3].x = int(0.35f*scr_w); rect_btn[3].y = rect_btn[2].y; rect_btn[3].w = rect_btn[0].w; rect_btn[3].h = rect_btn[0].h;
+	rect_btn[4].x = rect_btn[3].x; rect_btn[4].y = rect_btn[1].y; rect_btn[4].w = rect_btn[1].w; rect_btn[4].h = rect_btn[1].h;
+	rect_btn[5].x = rect_btn[3].x; rect_btn[5].y = rect_btn[0].y; rect_btn[5].w = rect_btn[0].w; rect_btn[5].h = rect_btn[0].h;
+	rect_center = { int(scr_w*0.3f),int(scr_h*0.4f),int(scr_w*0.4f),int(scr_h*0.2f) };
+	rect_stagefile.w = rect_stagefile.h = min(scr_w, scr_h) / 2;
+	rect_stagefile.x = (scr_w - rect_stagefile.w) / 2;
+	rect_stagefile.y = (scr_h - rect_stagefile.h) / 2;
+	rect_r.w = rect_l.w = rect_stagefile.x*0.75f;
+	rect_r.x = scr_w - rect_r.w; rect_l.x = 0;
+	rect_r.h = rect_l.h = rect_stagefile.w*0.6f;
+	rect_r.y = rect_l.y = (scr_h - rect_r.h) / 2;
+	rect_col.x = rect_btn[4].x + rect_btn[4].w; rect_col.w = rect_btn[1].x - rect_col.x;
+	SDL_Surface* tmp=NULL;
+	tmp = IMG_Load((string(curr_dir) + "\\image\\border.png").c_str());
+	border = SDL_CreateTextureFromSurface(render, tmp);
+	SDL_FreeSurface(tmp);
+	for (int i= 0; i < 6; i++) {
+		tmp = IMG_Load((string(curr_dir) + "\\image\\btn_" + to_string(i)+".png").c_str());
+		btn[i] = SDL_CreateTextureFromSurface(render,tmp);
+		SDL_FreeSurface(tmp);
+		tmp = IMG_Load((string(curr_dir) + "\\image\\btn_p_" + to_string(i)+".png").c_str());
+		btn_p[i] = SDL_CreateTextureFromSurface(render, tmp);
+		SDL_FreeSurface(tmp);
+		tmp = IMG_Load((string(curr_dir) + "\\image\\n_" + to_string(i) + ".png").c_str());
+		note[i] = SDL_CreateTextureFromSurface(render, tmp);
+		SDL_FreeSurface(tmp);
+		for (int j = 0; j < 3; j++) {
+			tmp = IMG_Load((string(curr_dir) + "\\image\\ln_" + to_string(i) + "_" + to_string(j) + ".png").c_str());
+			ln[i][j] = SDL_CreateTextureFromSurface(render, tmp);
+			SDL_FreeSurface(tmp);
+		}
+	}
+	int tex_w, tex_h;
+	for (int i = 0; i < 3; i++) {
+		SDL_QueryTexture(ln[0][i], NULL, NULL, &tex_w, &tex_h);
+		size_ln[i] = float(tex_w) / float(tex_h);
+	}
+	tmp = IMG_Load((string(curr_dir) + "\\image\\ln_end.png").c_str());
+	note[6] = SDL_CreateTextureFromSurface(render, tmp);
+	SDL_FreeSurface(tmp);
+	//image
+
+
+	Mix_AllocateChannels(72);
+	Mix_GroupChannels(0, 1, 0);
+	Mix_GroupChannels(2, 3, 1);
+	Mix_GroupChannels(4, 5, 2);
+	Mix_GroupChannels(6, 7, 3);
+	Mix_GroupChannels(8, 9, 4);
+	Mix_GroupChannels(10, 11, 5);
+	//group for button sound
+	Mix_GroupChannels(12, 61, 6);
+	Mix_GroupChannels(62, 71, 7);//for long sound
+	//group for bgm
+	//mixer
+
+	timer->start();
+}
+
+int Gamemain::loop() {//quit when you don't return 0
+	{
+		const Uint8 *curr_key_state = SDL_GetKeyboardState(NULL);
+		if (curr_key_state[SDL_SCANCODE_U]) {
+			input[0]++;
+		} else {
+			input[0] = (input[0]>0)?-1:0;
+		}
+		if (curr_key_state[SDL_SCANCODE_J]) {
+			input[1]++;
+		} else {
+			input[1] = (input[1] > 0) ? -1 : 0;
+		}
+		if (curr_key_state[SDL_SCANCODE_N]) {
+			input[2]++;
+		} else {
+			input[2] = (input[2] > 0) ? -1 : 0;
+		}
+		if (curr_key_state[SDL_SCANCODE_B]) {
+			input[3]++;
+		} else {
+			input[3] = (input[3] > 0) ? -1 : 0;
+		}
+		if (curr_key_state[SDL_SCANCODE_G]) {
+			input[4]++;
+		} else {
+			input[4] = (input[4] > 0) ? -1 : 0;
+		}
+		if (curr_key_state[SDL_SCANCODE_Y]) {
+			input[5]++;
+		} else {
+			input[5] = (input[5] > 0) ? -1 : 0;
+		}
+	}
+	//get button input(u,j,n,b,g,y->input[0]~input[5])
+
+	SDL_SetRenderDrawColor(render, 0x33, 0x33, 0x33, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(render);
+	SDL_RenderCopy(render, border, NULL, NULL);
+
+	switch (mode) {
+	case M_TITLE:title(); break;
+	case M_CHOOSE:choose(); break;
+	case M_LOADING:loading(); break;
+	case M_PLAY:play(); break;
+	case M_RESULT:result(); break;
+	}
+	SDL_RenderPresent(render);
+
+	return 0;
+}
+
+void Gamemain::close(){
+	for (int i = 0; i < 6; i++) {
+		SDL_DestroyTexture(btn[i]);
+		SDL_DestroyTexture(btn_p[i]);
+	}
+	bmsm.reset();
+	qr.close();
+	TTF_CloseFont(font);
+	TTF_Quit();
+}
---
+++ funcs.cpp
@@ -0,0 +1,400 @@
+#include "stdafx.h"
+#include "ColorfulHexa.h"
+#include <mmsystem.h>
+#include <fstream>
+
+//Timer
+void Timer::start(){
+	high = QueryPerformanceFrequency(&freq);
+	if (high) {
+		freq.QuadPart /= 1000l;
+		QueryPerformanceCounter(&start_time);
+		start_time.QuadPart /= freq.QuadPart;
+	}
+	else {
+		freq.QuadPart = 1;
+		start_time.QuadPart = timeGetTime();
+		timeBeginPeriod(1);
+	}
+	play_time.QuadPart = 0;
+	frames = 0;
+}
+void Timer::sleep(){
+	frames++;
+	if (high) {
+		QueryPerformanceCounter(&current_time);
+		current_time.QuadPart /= freq.QuadPart;
+	}
+	else {
+		current_time.QuadPart = timeGetTime();
+	}
+	play_time.QuadPart = current_time.QuadPart - start_time.QuadPart;
+	if (play_time.QuadPart < frames*frametime) {
+		Sleep((long)(frames*frametime)-(long)(play_time.QuadPart));
+	}
+	return;
+}
+void Timer::close(){
+	timeEndPeriod(1);
+}
+
+//QR_Reader
+int QR_Reader::init() {
+	return 0;
+}
+bool QR_Reader::read(){
+	return false;
+}
+void QR_Reader::reset(){}
+void QR_Reader::close(){}
+
+//BMS_Manager
+BMS_Manager::BMS_Manager(SDL_Renderer* render) {
+	auto th = thread([this,render] {
+		HANDLE h_dir, h_file;
+		WIN32_FIND_DATA dir_data, file_data;
+		char curr_dir[256] = {};
+		string dir_path;
+		string wildcards[] = { "*.bms","*.bme","*.bml" };
+		bool is_first;
+		GetModuleFileName(NULL, &curr_dir[0], 256);
+		char_traits<char>::copy(curr_dir, regex_replace(curr_dir, regex("\\\\[^\\\\]+$"), "").c_str(), 256);
+		h_dir = FindFirstFile((string(curr_dir) + "\\data\\*").c_str(), &dir_data);
+		if (h_dir == INVALID_HANDLE_VALUE) {
+			if (GetLastError() != ERROR_NO_MORE_FILES)error = -1;
+		}
+		while (error == 0) {
+			if (string(dir_data.cFileName).compare(".") != 0 && string(dir_data.cFileName).compare("..") != 0) {
+				is_first = true;
+				dir_path = string(curr_dir) + "\\data\\" + dir_data.cFileName + "\\";
+				for (int i = 0; i < 3; i++) {
+					error = 0;
+					h_file = FindFirstFile((dir_path + wildcards[i]).c_str(), &file_data);
+					if (h_file == INVALID_HANDLE_VALUE) {
+						continue;
+					}
+					while (error == 0) {
+						if (string(file_data.cFileName).compare(".") != 0 && string(file_data.cFileName).compare("..") != 0) {
+							if (is_first) { header.emplace_back(); }
+							error = header.back().load(dir_path, file_data.cFileName, is_first, render);
+							is_first = false;
+							if (!FindNextFile(h_file, &file_data) || error != 0) {
+								if (GetLastError() != ERROR_NO_MORE_FILES)error = -1;
+								header.back().sort();
+								FindClose(h_file);
+								break;
+							}
+						}
+					}
+				}
+			}
+			if (!FindNextFile(h_dir, &dir_data)) {
+				if (GetLastError() != ERROR_NO_MORE_FILES)error = -1;
+				FindClose(h_dir);
+				header_loaded = true;
+				break;
+			}
+		}
+		if (header.size() == 0) {
+			error = -1;
+		}
+	});
+	th.detach();
+	//load header
+}
+int BMS_Manager::Header::load(string dir,string file,bool is_first,SDL_Renderer* render) {
+	this->dir = dir;
+	file = dir + file;
+	bmsfile.push_back(file);
+	ifstream ifs(bmsfile.back());
+	string line,data;
+	bool has_total;
+
+	bar_num.push_back(1);
+	//set default
+
+	if (ifs.fail())return -1;
+	num++;
+	while(getline(ifs,line)){
+		if (line[0] != '#')continue;
+		line=regex_replace(line, regex(" \t"), "");
+		if (regex_search(line, regex("^#[0-9]{5}"))) {
+			if (stoi(line.substr(1, 3)) > bar_num.back())bar_num.back() = stoi(line.substr(1, 3))+1;
+		}
+		has_total = false;
+		for (int i= 0; i < 10; i++) {
+			if (!is_first && (i == 0 || i == 1 || i == 2 || i == 4 || i == 7 || i == 9))continue;
+			if (!regex_search(line, regex("^#" + cmd[i] + "[ \t]+")))continue;
+			data = regex_replace(line,regex("^#[^ \t]+[ \t]+"),"");
+			switch (i) {
+			case 0:
+				genre = data;
+				break;
+			case 1:
+				title = data;
+				break;
+			case 2:
+				artist = data;
+				break;
+			case 3:
+				bpm.push_back(data);
+				break;
+			case 4:
+				midifile = data;
+				break;
+			case 5:
+				playlevel.push_back(stoi(data));
+				break;
+			case 6:
+				rank.push_back(stoi(data));
+				break;
+			case 7:
+				volwav = stoi(data);
+				break;
+			case 8:
+				total.push_back(stoi(data));
+				has_total = true;
+				break;
+			case 9:
+				SDL_Surface* tmp = IMG_Load((dir + data).c_str());
+				stagefile = SDL_CreateTextureFromSurface(render,tmp);
+				SDL_FreeSurface(tmp);
+				break;
+			}
+		}
+	}
+	ifs.close();
+	if (bpm.size() < num)bpm.push_back("130");
+	if (!has_total)total.push_back(300l);
+	return 0;
+}
+void BMS_Manager::Header::sort() {
+	class Data {
+	public:
+		string bpm;
+		int playlevel;
+		int rank;
+		long total;
+		Data(string s_bpm, int i_playlevel, int i_rank, long l_total) {
+			bpm = s_bpm;
+			playlevel = i_playlevel;
+			rank = i_rank;
+			total = l_total;
+		}
+		Data(const Data& d){
+			bpm = d.bpm;
+			playlevel = d.playlevel;
+			rank = d.rank;
+			total = d.total;
+		}
+		~Data(){}
+		bool operator <(const Data& d) {
+			return playlevel < d.playlevel;
+		}
+		bool operator >(const Data& d) {
+			return playlevel > d.playlevel;
+		}
+	};
+	vector<Data> tmp;
+	for (int i= 0; i < num; i++) {
+		tmp.emplace_back(bpm[i], playlevel[i], rank[i], total[i]);
+	}
+	std::sort(tmp.begin(), tmp.end());
+	for (int i= 0; i < num; i++) {
+		bpm[i] = tmp[i].bpm;
+		playlevel[i] = tmp[i].playlevel;
+		rank[i] = tmp[i].rank;
+		total[i] = tmp[i].total;
+	}
+}
+int BMS_Manager::load_music(SDL_Renderer *render){
+	string file = header[sel].bmsfile[lv];
+	ifstream ifs(file);
+	string line;
+	int bar_no,channel;
+	vector<string> data;
+	unordered_map<string, long> ln_start, start_bar_no;
+	if (ifs.fail()) {
+		error = -1;
+		goto ending;
+	}
+
+	bar_count = vector<long>(header[sel].bar_num[lv], stoi(header[sel].bpm[lv])/130 * BAR_STANDARD);
+	music[2].emplace_back(header[sel].bpm[lv],0, 0l);
+	bar_count.push_back(0l);
+	//set initial data
+
+	while (getline(ifs, line)) {
+		line = regex_replace(line, regex("[ \t]+"), "");
+		if (!regex_search(line, regex("^#[0-9]{3}02:[0-9].?[0-9]+$")))continue;
+		bar_count[stoi(line.substr(1, 3))] = long(stof(line.substr(7))*float(BAR_STANDARD));
+	}
+	for (int i= 1; i < bar_count.size(); i++)bar_count[i] += bar_count[i - 1];
+	//get bar frame
+
+	ifs.clear();
+	ifs.seekg(0, std::ios::beg);
+
+	while(getline(ifs,line)){
+		line = regex_replace(line,regex("[ \t]+$"),"");
+		if (regex_search(line, regex("^#BMP[0-9]{2}"))) {
+			bmp[line.substr(4,2)]=SDL_CreateTextureFromSurface(render,IMG_Load(regex_replace(line, regex("#BMP[0-9]{2}[ \t]+"), "").c_str()));
+		}
+		if (regex_search(line, regex("^#WAV[0-9]{2}"))) {
+			string test = regex_replace(line, regex("#WAV[0-9]{2}[ \t]+"), "").c_str();
+			wav[line.substr(4, 2)] = Mix_LoadWAV((header[sel].dir+regex_replace(line, regex("#WAV[0-9]{2}[ \t]+"), "")).c_str());
+		}
+		if (regex_search(line, regex("^#BPM[0-9]{2}"))) {
+			bpm[line.substr(4, 2)] = stoi(regex_replace(line, regex("#BPM[0-9]{2}[ \t]+"), ""));
+		}
+
+		line = regex_replace(line, regex("[ \t]+"), ""); regex_replace(line, regex("#WAV[0-9]{2}[ \t]+"), "").c_str();
+		if (!regex_search(line,regex("^#[0-9]{5}:[0-9A-F]+$")))continue;
+
+		bar_no = stoi(line.substr(1, 3));
+		channel = stoi(line.substr(4, 2));
+
+		if ((51 > channel && channel >= 17)||channel==10||channel==9||channel==2)continue;
+		if (channel <= 8)channel--;
+		else if(channel<17)channel -= 3;
+		else channel -= 37;
+		//channel -> array key
+
+		string tmp_data = line.substr(7);
+		data.clear();
+		while (tmp_data.length() > 2) {
+			data.push_back(tmp_data.substr(0, 2));
+			tmp_data = tmp_data.substr(2);
+		}
+		data.push_back(tmp_data);
+		tmp_data = "";
+		//split data into unit (2 digits)
+
+		for (unsigned i = 0; i < data.size(); i++) {
+			if (data[i] == "00")continue;
+			if (channel < 14) {
+				music[channel].emplace_back(data[i], bar_no, (long)(bar_count[(bar_no == 0) ? 0 : (bar_no - 1)] + i * (bar_count[bar_no] - bar_count[(bar_no == 0) ? 0 : (bar_no - 1)]) / data.size()));
+				num_note++;
+			}
+			//for normal notes
+
+			else {
+				if (ln_start.count(data[i]) == 1 && ln_start[data[i]] > 0) {//for end note
+					music[channel].emplace_back(data[i], start_bar_no[data[i]], ln_start[data[i]] , (long)(bar_count[bar_no - 1] + i * (bar_count[bar_no] - bar_count[bar_no - 1]) / data.size()));
+					num_note++;
+					ln_start[data[i]] = -1;
+				} else {//for start note
+					start_bar_no[data[i]] = bar_no;
+					ln_start[data[i]] = (long)(bar_count[bar_no - 1] + i * (bar_count[bar_no] - bar_count[bar_no - 1]) / data.size());
+				}
+			}
+			//for long notes
+		}
+		//put data into array
+	}
+	//get notes data
+
+	ifs.close();
+
+	for (int i= 0; i < NUM_CHANNEL; i++) {
+		sort(music[i].begin(), music[i].end());
+	}
+	//sort notes
+	set_time_bpm_change();
+
+	ending:
+	music_loaded = true;//tell thread ending
+	return 0;
+}
+void BMS_Manager::reset() {
+	for (int i= 0; i < NUM_CHANNEL;i++) {
+		music[i].clear();
+	}
+	for (auto itr = wav.begin(), end = wav.end(); itr != end; ++itr) {
+		Mix_FreeChunk(itr->second);
+	}
+	wav.clear();
+	for (auto itr = bmp.begin(), end = bmp.end(); itr != end; ++itr) {
+		SDL_DestroyTexture(itr->second);
+	}
+	bmp.clear();
+	bpm.clear();
+	bar_count.clear();
+	fill_n(next_note, NUM_CHANNEL, 0);
+	num_note = 0;
+	sel = 0;
+	lv = 0;
+	speed = 1.0f;
+	time_draw_start = 1000l;
+	error = 0;
+}
+void BMS_Manager::get_notes(vector<BMS_Manager::ENote> en[],long time) {
+	for (int i= 0; i < 13; i++) {
+		if(i<7)en[i].clear();
+		if (music[(i==0)?0:i+7].size() <= next_note[(i==0)?0:i+7])continue;
+		for (int j = next_note[(i==0)?0:i+7];music[(i==0)?0:i+7][j].get_count()<time_to_count(time + time_draw_start); j++) {
+			en[(i<7)?i:i-7].emplace_back(music[(i==0)?0:i + 7][j], j);
+			if (j >= music[(i==0)?0:i+7].size()-1)break;
+		}
+	}
+	for (int i= 0; i < 6; i++) {
+		if (en[i].size() == 0)continue;
+		sort(en[i].begin(), en[i].end());
+	}
+}
+void BMS_Manager::set_time_bpm_change() {
+	time_bpm_change.clear();
+	time_bpm_change.reserve(music[2].size());
+	time_bpm_change.push_back(0l);
+	long time = 0l;
+	for (int i=1;i<music[2].size();i++){
+		time = time_bpm_change[i - 1];
+		time += (music[2][i].get_count() - music[2][i - 1].get_count())/BAR_STANDARD*4/stol(music[2][i].get_data(),NULL,16)*60;
+		time_bpm_change.push_back(time);
+	}
+	return;
+}
+long BMS_Manager::time_to_count(long time) {
+	int i;
+	for (i= 0;i<time_bpm_change.size()-1; i++) {
+		if (time_bpm_change[i+1] > time)break;
+	}
+	return music[2][i].get_count() + long(stol(music[2][i].get_data(),NULL,16)/60.0*double(time - time_bpm_change[i])/1000.0*double(BAR_STANDARD)/4.0);
+}
+
+//algorithms
+int min_l(initializer_list<int> args) {
+	int min = INT_MAX;
+	for (int x : args) {
+		min = min(min, x);
+	}
+	return min;
+}
+int max_l(initializer_list<int> args) {
+	int max = INT_MIN;
+	for (int x : args) {
+		max = max(max, x);
+	}
+	return max;
+}
+float min_l(initializer_list<float> args) {
+	float min = FLT_MAX;
+	for (float x : args) {
+		min = min(min, x);
+	}
+	return min;
+}
+float max_l(initializer_list<float> args) {
+	float max = FLT_MIN;
+	for (float x : args) {
+		max = max(max, x);
+	}
+	return max;
+}
+int SDL_RenderCopyAlpha(SDL_Renderer *render, SDL_Texture *texture, const SDL_Rect *srcrect, const SDL_Rect *dstrect, unsigned int alpha) {
+	if (alpha > 255)return -1;
+	if(SDL_SetTextureAlphaMod(texture, alpha)!=0)return -1;
+	SDL_RenderCopy(render, texture, srcrect, dstrect);
+	SDL_SetTextureAlphaMod(texture, 255);
+	return 0;
+}
